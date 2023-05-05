51f1868ce4aef867fea05c65abf91e4611542fdb
Mulyarchuk Elena <ml.mulyarchuk@gmail.com>
2023-05-05 23:35:03 +0200

    allHW

---
+++ contacts.js
@@ -0,0 +1,43 @@
+const fs = require("fs/promises");
+const path = require("path");
+const { nanoid } = require("nanoid");
+
+const contactsPath = path.join(__dirname,`db`,`contacts.json`);
+
+
+async function listContacts() {
+    const contacts = await fs.readFile(contactsPath);
+    return JSON.parse(contacts);
+  }
+  
+async function getContactById(contactId) {
+    const contacts = await listContacts();
+    const contactsById = contacts.find(item => item.id === contactId);
+    return contactsById || null;
+  }
+  
+async function removeContact(contactId) {
+    const contacts = await listContacts();
+    const index = contacts.findIndex(item=> item.id ===contactId);
+    if (index===-1){
+        return null;
+    }
+    const [result] = contacts.splice(index, 1);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return result;
+  }
+  
+async function addContact(name, email, phone) {
+    const contacts = await listContacts();
+    const contact = {id:nanoid(), name, email, phone} ;
+    contacts.push(contact);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts));
+    return contact;
+  }
+
+  module.exports = {
+    listContacts,
+    getContactById,
+    removeContact,
+    addContact
+  }
